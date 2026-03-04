5e84115a29ec17083177c3af4a814ca2dd6e45c5
GPR <2300031613@kluniversity.in>
2026-03-04 11:06:21 +0530

    code1

--- src/main/java/com/klef/model/Student.java
+++ src/main/java/com/klef/model/Student.java
@@ -6,85 +6,66 @@ import jakarta.persistence.Id;
 import jakarta.persistence.Table;
 
 @Entity
-@Table(name ="Student_table")
-public class Student {
-
-	  //properties
-	  @Id //primary key=unique + notnull
-	  @Column(name = "sid")
-	  private long id;
-	  @Column(name="sname",length=50,nullable = false)
-	  private String name;
-	  @Column(nullable = false)
-	  private String sgender;
-	  @Column(nullable = false)
-	  private String dep;
-	  @Column(name = "semail",length = 250,nullable = false,unique = true)
-	  private String email;
-	  @Column(name= "scontact",length = 15,nullable = false,unique = true)
-	  private String contact;
-	  
-	  @Column(length=30,nullable = false)
-	  private String password;
-
-	public long getId() {
-		return id;
-	}
-
-	public void setId(long id) {
-		this.id = id;
-	}
-
-	public String getName() {
-		return name;
-	}
-
-	public void setName(String name) {
-		this.name = name;
-	}
-
-	public String getSgender() {
-		return sgender;
-	}
-
-	public void setSgender(String sgender) {
-		this.sgender = sgender;
-	}
-
-	public String getDep() {
-		return dep;
-	}
-
-	public void setDep(String dep) {
-		this.dep = dep;
-	}
-
-	public String getEmail() {
-		return email;
-	}
-
-	public void setEmail(String email) {
-		this.email = email;
-	}
-
-	public String getContact() {
-		return contact;
-	}
-
-	public void setContact(String contact) {
-		this.contact = contact;
-	}
-
-	public String getPassword() {
-		return password;
-	}
-
-	public void setPassword(String password) {
-		this.password = password;
-	}
-	  
-	  
-	  
-	  
-	  
+@Table(name = "student_table")
+public class Student 
+{
+  //properties
+	@Id //primarykey = unique + notnull
+	@Column(name = "sid" )
+   private long id;
+	@Column(name = "sname",length = 50,nullable = false)
+   private String name;
+	@Column(nullable = false)
+   private String gender;
+	@Column(nullable = false)
+   private String department;
+	@Column(name = "semail",length = 50,nullable = false,unique = true)
+   private String email;
+	@Column(name="scontact",length = 15,nullable = false,unique = true)
+   private String contact;
+	@Column(length = 30,nullable = false)
+	private String password;
+   //setter and getter methods 
+public long getId() {
+	return id;
+}
+public void setId(long id) {
+	this.id = id;
+}
+public String getName() {
+	return name;
+}
+public void setName(String name) {
+	this.name = name;
+}
+public String getGender() {
+	return gender;
+}
+public void setGender(String gender) {
+	this.gender = gender;
+}
+public String getDepartment() {
+	return department;
+}
+public void setDepartment(String department) {
+	this.department = department;
+}
+public String getEmail() {
+	return email;
+}
+public void setEmail(String email) {
+	this.email = email;
+}
+public String getContact() {
+	return contact;
+}
+public void setContact(String contact) {
+	this.contact = contact;
+}
+public String getPassword() {
+	return password;
+}
+public void setPassword(String password) {
+	this.password = password;
+}
 }
